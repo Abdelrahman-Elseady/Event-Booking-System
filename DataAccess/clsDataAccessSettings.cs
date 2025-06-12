7b0fc38ac08426e6b861a5cb6dde6413f64af285
Abdo Elseady <abdoelseady10@gmail.com>
2025-06-12 11:00:19 +0300

    class clsEventData was added

---
+++ DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class clsDataAccessSettings
+    {
+        public static string ConnectionString = "server=.;DataBase=Event_Booking;User id=sa;Password=sa123456";
+    }
+}
