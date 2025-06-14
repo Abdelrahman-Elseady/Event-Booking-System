6a051dc207941f5c2c8b13383c0b96d83f4da474
Abdo Elseady <abdoelseady10@gmail.com>
2025-06-14 03:15:55 +0300

    Data Access and Business Layer Completed

---
+++ DataAccess/clsEventCategoryData.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class clsEventCategoryData
+    {
+        public static int AddNewCategory(string name, string description)
+        {
+            int id = -1;
+
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand("sp_AddNewCategory", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter outId = new SqlParameter("@Event_Category_ID", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+
+                cmd.Parameters.Add(outId);
+                cmd.Parameters.AddWithValue("@Category_Name", name);
+                cmd.Parameters.AddWithValue("@Category_Description", description ?? (object)DBNull.Value);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    id = (int)cmd.Parameters["@Event_Category_ID"].Value;
+                }
+                catch { id = -1; }
+            }
+
+            return id;
+        }
+
+        public static bool UpdateCategory(int id, string name, string description)
+        {
+            bool updated = false;
+
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand("sp_UpdateCategory", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Event_Category_ID", id);
+                cmd.Parameters.AddWithValue("@Category_Name", name);
+                cmd.Parameters.AddWithValue("@Category_Description", description ?? (object)DBNull.Value);
+
+                try
+                {
+                    conn.Open();
+                    updated = cmd.ExecuteNonQuery() > 0;
+                }
+                catch { updated = false; }
+            }
+
+            return updated;
+        }
+
+        public static bool DeleteCategory(int id)
+        {
+            bool deleted = false;
+
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand("sp_DeleteCategory", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Event_Category_ID", id);
+
+                try
+                {
+                    conn.Open();
+                    deleted = cmd.ExecuteNonQuery() > 0;
+                }
+                catch { deleted = false; }
+            }
+
+            return deleted;
+        }
+
+        public static bool FindCategory(int id, ref string name, ref string description)
+        {
+            bool found = false;
+
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand("sp_FindCategory", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Event_Category_ID", id);
+
+                try
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            name = reader["Category_Name"].ToString();
+                            description = reader["Category_Description"]?.ToString();
+                            found = true;
+                        }
+                    }
+                }
+                catch { found = false; }
+            }
+
+            return found;
+        }
+
+        public static DataTable GetAllCategories()
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand("sp_GetAllCategories", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        dt.Load(reader);
+                    }
+                }
+                catch { }
+            }
+
+            return dt;
+        }
+    }
+
+}
---
+++ EBS_Business/clsAccount.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace EBS_Business
+{
+    public class clsAccount
+    {
+        public enum enMode { AddNew = 0, Update = 1 }
+        enMode Mode;
+
+        public int AccountID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+
+        public clsAccount()
+        {
+            Mode = enMode.AddNew;
+            AccountID = -1;
+            UserName = Password = string.Empty;
+            IsActive = true;
+        }
+
+        public clsAccount(int accountID, string userName, string password, bool isActive)
+        {
+            Mode = enMode.Update;
+            AccountID = accountID;
+            UserName = userName;
+            Password = password;
+            IsActive = isActive;
+        }
+
+        private bool _AddNewAccount()
+        {
+            this.AccountID = clsAccountData.AddNewAccount(this.UserName, this.Password, this.IsActive);
+            return this.AccountID != -1;
+        }
+
+        private bool _UpdateAccount()
+        {
+            return clsAccountData.UpdateAccount(this.AccountID, this.UserName, this.Password, this.IsActive);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewAccount())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    break;
+                case enMode.Update:
+                    return _UpdateAccount();
+            }
+
+            return false;
+        }
+
+        public static bool Delete(int id)
+        {
+            return clsAccountData.DeleteAccount(id);
+        }
+
+        public static clsAccount GetAccountByID(int id)
+        {
+            string userName = string.Empty;
+            string password = string.Empty;
+            bool isActive = false;
+
+            if (clsAccountData.FindAccount(id, ref userName, ref password, ref isActive))
+            {
+                return new clsAccount(id, userName, password, isActive);
+            }
+
+            return new clsAccount(); // not found
+        }
+
+        public static DataTable GetAllAccounts()
+        {
+            return clsAccountData.GetAllAccounts();
+        }
+    }
+
+}
